42fd6a0192399836cd4fc53f7ad4b8609eb82678
Alex Whitman <alex@alexwhitman.com>
2011-11-18 16:24:15 +0000

    Remove bot.Module.load() - no longer required
    - options are now assigned to module rather than passed in

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 module.exports = function(bot, module) {
 
-	module.load = function() {
-		var words = [
-			'arse',
-			'ass',
-			'bastard',
-			'bugger',
-			'bollocks',
-			'bullshit',
-			'cock',
-			'cunt',
-			'damn',
-			'dick',
-			'douche',
-			'fag',
-			'fuck',
-			'fucked',
-			'piss',
-			'shit',
-			'wank'
-		];
-		var regex = new RegExp('(?:^|\\s)(' + words.join('|') + ')(?:\\s|\\.|\\?|!|$)', 'i');
-		module.addTrigger(regex, function(request) {
-			request.reply = 'You have been fined one credit for a violation of the verbal morality statute.';
-			if (request.multiuser && request.usernick) {
-				request.reply = request.usernick + ': ' + request.reply;
-			}
-			bot.respond(request);
-		});
-	};
+	var words = [
+		'arse',
+		'ass',
+		'bastard',
+		'bugger',
+		'bollocks',
+		'bullshit',
+		'cock',
+		'cunt',
+		'damn',
+		'dick',
+		'douche',
+		'fag',
+		'fuck',
+		'fucked',
+		'piss',
+		'shit',
+		'wank'
+	];
+	var regex = new RegExp('(?:^|\\s)(' + words.join('|') + ')(?:\\s|\\.|\\?|!|$)', 'i');
+	module.addTrigger(regex, function(request) {
+		request.reply = 'You have been fined one credit for a violation of the verbal morality statute.';
+		if (request.multiuser && request.usernick) {
+			request.reply = request.usernick + ': ' + request.reply;
+		}
+		bot.respond(request);
+	});
 
 };
